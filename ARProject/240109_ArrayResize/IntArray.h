5db75af7f6133e05198ca76dca05d509e0401ca6
kjb <kjb_jh@naver.com>
2024-01-09 10:44:37 +0900

    Add Function and Variable.
    # 생성자, 소멸자, 함수 선언, 변수 선언 및 초기화.

--- ARProject/240109_ArrayResize/IntArray.h
+++ ARProject/240109_ArrayResize/IntArray.h
@@ -1,5 +1,17 @@
 #pragma once
 class IntArray
 {
+public :
+	IntArray(int _Size);
+	~IntArray();
+
+	void ReSize(int _Size);
+	void Release();
+
+	inline int Num();	
+	
+private :
+	int NumValue = -1;
+	int* ArrPtr = nullptr;
 };
 
