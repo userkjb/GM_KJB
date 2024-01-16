651f09e853eb65bdf0ca74187206f6d173e5d129
kjb <kjb_jh@naver.com>
2024-01-16 15:44:19 +0900

    default BodyManager Class
    # BodyManager 기반 완료.

--- ARProject/240116_SnakeTest/BodyManager.h
+++ ARProject/240116_SnakeTest/BodyManager.h
@@ -1,5 +1,22 @@
 #pragma once
-class BodyManager
+#include <ConsoleEngine/ConsoleUpdater.h>
+
+class BodyManager : public ConsoleUpdater
 {
+public :
+	static class Body* GetCurBody()
+	{
+		return CurBody;
+	}
+
+	static void ResetBody()
+	{
+		CurBody = nullptr;
+	}
+
+	void Update() override;
+
+private :
+	static class Body* CurBody;
 };
 
