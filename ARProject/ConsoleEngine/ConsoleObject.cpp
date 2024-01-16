81cb9a4f90b73e011484c02bd4411cae058fb31a
kjb <kjb_jh@naver.com>
2024-01-16 15:16:01 +0900

    Update ConsoleObject
    # ConsoleObject 수정.

--- ARProject/ConsoleEngine/ConsoleObject.cpp
+++ ARProject/ConsoleEngine/ConsoleObject.cpp
@@ -1,4 +1,5 @@
 #include "ConsoleObject.h"
+#include "EngineCore.h"
 
 ConsoleObject::ConsoleObject()
 {
@@ -13,4 +14,34 @@ ConsoleObject::ConsoleObject(const int2& _StartPos, char _RenderChar)
 ConsoleObject::~ConsoleObject()
 {
 
+}
+
+ConsoleObject* ConsoleObject::Collision(int _UpdateOrder)
+{
+    std::list<ConsoleObject*>& ObjectList = GetCore()->AllUpdateObject[_UpdateOrder];
+
+    std::list<ConsoleObject*>::iterator StartIter = ObjectList.begin();
+    std::list<ConsoleObject*>::iterator EndIter = ObjectList.end();
+    for (; StartIter != EndIter; ++StartIter)
+    {
+        ConsoleObject* Object = *StartIter;
+
+        if (nullptr == Object)
+        {
+            MsgBoxAssert("오브젝트가 nullptr인 경우가 존재합니다.");
+        }
+
+        if (this == Object)
+        {
+            continue;
+        }
+
+        // 이게 충돌
+        if (GetPos() == Object->GetPos())
+        {
+            return Object;
+        }
+    }
+
+    return nullptr;
 }
--- ARProject/ConsoleEngine/ConsoleObject.h
+++ ARProject/ConsoleEngine/ConsoleObject.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "ConsoleMath.h"
+#include "ConsoleUpdater.h"
 
-class ConsoleObject
+class ConsoleObject : public ConsoleUpdater
 {
+	friend class EngineCore;
+
 public :
 	ConsoleObject();
 	ConsoleObject(const int2& _StartPos, char _RenderChar);
@@ -31,6 +34,16 @@ public :
 	{
 		Pos += _Dir;
 	}
+
+	template<typename EnumType>
+	ConsoleObject* Collision(EnumType _UpdateOrder)
+	{
+		return Collision(static_cast<int>(_UpdateOrder));
+	}
+
+	// 나랑 충돌한 오브젝트
+	ConsoleObject* Collision(int _UpdateOrder);
+
 
 private :
 	int2 Pos = { 0, 0 };
