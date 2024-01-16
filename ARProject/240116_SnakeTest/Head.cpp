dcb6d4a175aeec02eccb3045831d63406455b28d
kjb <kjb_jh@naver.com>
2024-01-16 15:46:24 +0900

    default Head class
    # Head 클래스 기반 완료.

--- ARProject/240116_SnakeTest/Head.cpp
+++ ARProject/240116_SnakeTest/Head.cpp
@@ -1,5 +1,60 @@
 #include "Head.h"
+#include <conio.h>
+#include <ConsoleEngine/EngineCore.h>
+#include "BodyManager.h"
+#include "Body.h"
 
 void Head::Update()
 {
+	int InputCount = _kbhit();
+	if (0 == InputCount)
+	{
+		return;
+	}
+
+	int Select = _getch();
+
+	// InputCount = _kbhit();
+
+	// X Y
+	// 1 0
+
+	switch (Select)
+	{
+	case 'A':
+	case 'a':
+		AddPos(Left);
+		break;
+	case 'S':
+	case 's':
+		AddPos(Down);
+		break;
+	case 'W':
+	case 'w':
+		AddPos(Up);
+		break;
+	case 'D':
+	case 'd':
+		AddPos(Right);
+		break;
+	case '1':
+		GetCore()->EngineEnd();
+		break;
+	default:
+		break;
+	}
+
+	if (nullptr == BodyManager::GetCurBody())
+	{
+		MsgBoxAssert("먹을수 있는 바디가 존재하지 않습니다.");
+		return;
+	}
+
+	Body* CurBody = BodyManager::GetCurBody();
+
+	if (CurBody->GetPos() == GetPos())
+	{
+		Back = CurBody;
+		BodyManager::ResetBody();
+	}
 }
